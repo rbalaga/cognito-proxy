1ea57dd30def8aa90f866d93f305d9c001dadbd5
RAMMOHANA RAO BALAGA <ramanji.balaga@gmail.com>
2021-12-02 16:04:07 +0530

    server code update

--- index.js
+++ index.js
@@ -4,6 +4,101 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 
+app.use(
+		"/login",
+		createProxyMiddleware({
+			target: "https://pace-ram.auth.us-east-1.amazoncognito.com",
+			changeOrigin: true,
+			selfHandleResponse: true,
+			onProxyReq: (proxyReq, req, res) => {
+				console.log("req headers", req.headers);
+				console.log("proxy req headers", proxyReq.headers);
+			},
+			onProxyRes: responseInterceptor(async (responseBuffer, proxyRes, req, res) => {
+				// detect json responses
+				res.removeHeader("X-Frame-Options");
+				console.log("location header verification");
+        let cookiesArr = res.getHeader("set-cookie");
+        if (cookiesArr) {
+          cookiesArr = cookiesArr.map((cookie) => {
+            if (cookie.includes('SameSite')) {
+               cookie = cookie.replace('=Lax', '=None'); 
+            } else {
+              cookie += '; SameSite=None';
+            }
+            cookie = cookie.replace('Secure', 'Secure=false');
+            return cookie;
+          });
+          res.setHeader("set-cookie", cookiesArr);
+        }
+				console.log(res.getHeaderNames());
+				if (res.hasHeader("location")) {
+					const location = res.getHeader("location");
+					res.removeHeader("location");
+					res.setHeader(
+						"location",
+						location.replace(
+							"https://search-connect-ctr-agent-records-orcax4mgypqgo7ho2yze6eqwtm.us-east-1.es.amazonaws.com/_dashboards",
+							"_dashboards",
+						),
+					);
+				}
+				// if (proxyRes.headers["content-type"]?.includes("text/html")) {
+				// 	console.log("html response received proxy");
+				// 	let data = responseBuffer.toString("utf8");
+				// 	return data;
+				// } else {
+				// }
+				return responseBuffer; // manipulate response and return the result
+			}),
+		}),
+	);
+
+	// app.post("/login", (req, res) => res.end(req.body));
+
+	app.use(
+		"/_dashboards",
+		createProxyMiddleware({
+			target: "https://search-connect-ctr-agent-records-orcax4mgypqgo7ho2yze6eqwtm.us-east-1.es.amazonaws.com/",
+			changeOrigin: true,
+			selfHandleResponse: true,
+			onProxyRes: responseInterceptor(async (responseBuffer, proxyRes, req, res) => {
+				// detect json responses
+				res.removeHeader("X-Frame-Options");
+				console.log("location header verification");
+        let cookiesArr = res.getHeader("set-cookie");
+        if (cookiesArr) {
+          cookiesArr = cookiesArr.map((cookie) => {
+            if (cookie.includes('SameSite')) {
+               cookie = cookie.replace('=Lax', '=None'); 
+            } else {
+              cookie += '; SameSite=None';
+            }
+            cookie = cookie.replace('Secure', 'Secure=false');
+            return cookie;
+          });
+          res.setHeader("set-cookie", cookiesArr);
+        }
+				if (res.hasHeader("location")) {
+					const location = res.getHeader("location");
+					res.removeHeader("location");
+					// console.log("location", location);
+					res.setHeader(
+						"location",
+						location.replace("https://pace-ram.auth.us-east-1.amazoncognito.com/login", "/login"),
+					);
+				}
+				// if (proxyRes.headers["content-type"]?.includes("text/html")) {
+				// 	console.log("html response received proxy");
+				// 	let data = responseBuffer.toString("utf8");
+				// 	return data;
+				// } else {
+				// }
+				return responseBuffer; // manipulate response and return the result
+			}),
+		}),
+	);
+
 const cognitoLoginProxy = createProxyMiddleware({
   target: "https://pace-ram.auth.us-east-1.amazoncognito.com",
   changeOrigin: true,
@@ -69,14 +164,14 @@ const openSearchDashboardProxy = createProxyMiddleware({
   }),
 });
 
-app.use("/login", cognitoLoginProxy);
-app.use("/logout", cognitoLoginProxy);
-app.use("/_dashboards/*", openSearchDashboardProxy);
+// app.use("/login", cognitoLoginProxy);
+// app.use("/logout", cognitoLoginProxy);
+// app.use("/_dashboards/*", openSearchDashboardProxy);
 app.use("/ram", (req, res) => {
   res.end("Hello RAM from cognito proxy");
 });
 app.use("/", (req, res) => {
-  res.end("Hello from cognito proxy");
+  res.end("Hello from cognito proxy v2");
 });
 const port = process.env.PORT;
 app.listen(port, () => {
